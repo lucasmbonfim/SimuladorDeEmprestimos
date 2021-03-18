8c89464a86145c1a44566dfb4d57a18eba6b912d
lucasmbonfim <79766250+lucasmbonfim@users.noreply.github.com>
2021-03-18 15:06:23 -0300

    Update 3 return.php
    Finalizando corretamente a validação do CPF (Quantidade de digitos)

--- return.php
+++ return.php
@@ -2,31 +2,44 @@
 <head>
 
 	<style>
+		
 		#retornar{
 			color: white;
             font-family: sans-serif
 		}
-        	body{
-            		text-align: center;
-            		background:blue;
-            		color: white;
+
+        body{
+            text-align: center;
+            background:blue;
+            color: white;
         }
+
+
 	</style>
 
 </head>
 
+<body>
 <?php
 
 $nome = $_POST['nome'];
 $cpf = $_POST['cpf'];
 
-echo "<h1>".$nome. " <br>CPF do usuário: ".$cpf. "<br></h2>";
+echo "<h1>".$nome. " | CPF do usuário: ".$cpf. "<br></h2>";
 
 	function isCpf($cpf){
 		$cpf = preg_replace('/[^0-9]/','',$cpf); // pegando somente os numeros do cpf 
 
 		$dig1 = 0;
 		$dig2 = 0;
+
+		$limite=strlen($cpf);
+		if ($limite<11 || $limite>14)
+		{
+			echo "<h1>Quantidades de digitos do CPF inválida!</h1>";
+
+			return false;
+		}
 
 		for ($i=0, $x=10; $i<=8;$i++,$x--){
 			$dig1 += $cpf[$i] * $x;
@@ -59,21 +72,11 @@ echo "<h1>".$nome. " <br>CPF do usuário: ".$cpf. "<br></h2>";
 		include("C:/xampp/htdocs/POO2021_4termo/simuemp/simulador.php");
 	
 	}else{
-		echo " Tente novamente. Revise o CPF inserido";
+		echo "<h2>Tente novamente. Revise o CPF inserido</h2>";
 	}
 
-/*
-Para validar o CPF
-Considere uma string de 11 caracteres para armazenar sem pontos ou traço, apenas números.
-A verificação do CPF está em calcular os dois dígitos de controle e comparar com os digitados, se os dois calculados forem iguais aos dois digitados, o CPF digitado é válido, caso contrário, o CPF é inválido.
-Cálculo do 1º dígito: Soma os 9 primeiros números do CPF multiplicados de 1 a 9 e calcula-se o resto desta soma por 11. Se o resto for igual a 10, então o dígito é 0.
-Cálculo do 2º dígito: Soma os 9 primeiros números do CPF multiplicados de 9 a 1 e calcula-se o resto desta soma por 11. Se o resto for igual a 10, então o dígito é 0.
-Após o cálculo, compara-se com as respectivas posições da string e informe se o CPF é válido ou inválido.
-Colocar um link para voltar para nova simulação.
-*/
-
-
 ?>
+</body>
 <html>
 <br><br>
 
